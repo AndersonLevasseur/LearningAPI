6ee2176fc6ca8a5fd21d2bc15eef4d2d96623701
Anderson Levasseur <anderson.levasseur@gmail.com>
2021-06-14 22:10:58 -0400

    works with minor error on sync

--- jokeAPI.js
+++ jokeAPI.js
@@ -1,10 +1,76 @@
-function async(){
-    fetch("https://v2.jokeapi.dev/joke/pun").then()
+document.getElementById("clear").addEventListener("click", clear);
+document.getElementById("async").addEventListener("click", async);
+document.getElementById("sync").addEventListener("click", sync);
+
+function clear() {
+    document.getElementById("cell1").innerHTML = "not started";
+    document.getElementById("cell2").innerHTML = "not started";
+    document.getElementById("cell3").innerHTML = "not started";
+    document.getElementById("cell4").innerHTML = "not started";
+    console.clear();
 }
 
-function sync() {
-    Promise.all(fetch)
+function pending() {
+    document.getElementById("cell1").innerHTML = "pending";
+    document.getElementById("cell2").innerHTML = "pending";
+    document.getElementById("cell3").innerHTML = "pending";
+    document.getElementById("cell4").innerHTML = "pending";
 }
+
+function async() {
+    pending();
+    // make it able to run for number of jokes wanted?, would totally change html though
+    fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single")
+        .then(response => response.json())
+        .then(json => json.joke)
+        /*replace line below with html editing*/
+        .then(data => {
+            document.getElementById("cell1").innerHTML = `${data}`;
+            return fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single")
+        })
+        .then(response => response.json())
+        .then(json => json.joke)
+        /*replace line below with html editing*/
+        .then(data => {
+            document.getElementById("cell2").innerHTML = `${data}`;
+            return fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single")
+        })
+        .then(response => response.json())
+        .then(json => json.joke)
+        /*replace line below with html editing*/
+        .then(data => {
+            console.log(data);
+            return fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single")
+        })
+        .then(response => response.json())
+        .then(json => json.joke)
+        /*replace line below with html editing*/
+        .then(data => {
+            console.log(data);
+        })
+        .catch(console.log("FAIL"));
+}
+
+let arr = [
+    fetch("https://v2.jokeapi.dev/joke/Any?type=single"),
+    fetch("https://v2.jokeapi.dev/joke/Any?type=single"),
+]
+
+function sync() {
+    pending();
+    Promise.all(arr)
+        .then(response => {
+            for (let i = 1; i < 5; i++) {
+                response[i].json()
+                    .then((data) => data.joke)
+                    .then(data => {
+                        document.getElementById(`cell${i}`).innerHTML = `${data}`;
+                    })
+            }
+        })
+        .catch(console.log("FAIL"));
+}
+
 /*
 async
 
